7ec09db9a2909c0347a9ea2c6095c9a7a2fa36d2
frakzzPUC <francisco.cardoso19@hotmail.com>
2023-06-01 19:14:55 -0300

    Add Click na lista + Começando Tela de Loc

--- lib/dentista_view.dart
+++ lib/dentista_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_functions/cloud_functions.dart';
 import 'dart:convert'; // Importante para o jsonDecode.
+import 'package:flutter_itooth/loc_view.dart'; // Importe sua tela loc_view.dart
 
 class DentistaView extends StatefulWidget {
   @override
@@ -8,38 +9,25 @@ class DentistaView extends StatefulWidget {
 }
 
 class _DentistaViewState extends State<DentistaView> {
-  // Variável para guardar os dados do Firebase.
   List<dynamic> _firebaseData = [];
 
   @override
   void initState() {
     super.initState();
-
-    // Buscar os dados do Firebase.
     _getFirebaseData();
   }
 
-  // Função para buscar os dados do Firebase.
   _getFirebaseData() async {
-    // Vamos chamar a função getEmergencyAcceptedUsers ao invés de buscar diretamente do Firestore.
     HttpsCallable callable = FirebaseFunctions.instanceFor(region: 'southamerica-east1').httpsCallable('getEmergencyAcceptedUsers');
     try {
       final results = await callable();
-      print('Dados recebidos do Firebase: ${results.data}');
-      // Parse the JSON string into a dynamic object.
       dynamic response = jsonDecode(results.data);
-
-      // Check the 'status' field to make sure the request was successful.
       if (response['status'] == 'SUCCESS') {
-        // Decode the 'payload' field, which is already a list.
         List<dynamic> users = response['payload'];
-
-        // Use setState to tell Flutter to rebuild the UI with the new data.
         setState(() {
           _firebaseData = users;
         });
       } else {
-        // Handle the error.
         print('Error getting data: ${response['message']}');
       }
     } catch (e) {
@@ -53,50 +41,79 @@ class _DentistaViewState extends State<DentistaView> {
       body: Column(
         children: [
           Image.asset(
-            'assets/images/itooth.png', // Caminho para a imagem da logo do seu app
+            'assets/images/itooth.png',
             width: MediaQuery.of(context).size.width * 0.4,
             height: MediaQuery.of(context).size.height * 0.2,
             alignment: Alignment.center,
           ),
           Expanded(
-            child: _firebaseData.isNotEmpty ?
-            ListView.builder(
+            child: _firebaseData.isNotEmpty
+                ? ListView.builder(
               itemCount: _firebaseData.length,
               itemBuilder: (BuildContext context, int index) {
-                return Card(
-                  margin: EdgeInsets.all(10.0), // Espaçamento de um usuário para o outro.
-                  child: Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: Row(
-                      children: [
-                        // Aqui está a imagem do usuário.
-                        CircleAvatar(
-                          backgroundImage: AssetImage('assets/images/default_user.png'), // Aqui é onde você colocaria o URL da foto do usuário.
-                          radius: 30.0,
-                        ),
-                        SizedBox(width: 10.0), // Espaço entre a imagem e as informações do usuário.
-                        Flexible(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                'Nome: ${_firebaseData[index]['name'] ?? 'Nome desconhecido'}', // Aqui estão os nomes dos usuários.
-                                style: TextStyle(fontSize: 24.0),
-                              ),
-                              Text(
-                                'Telefone: ${_firebaseData[index]['phone'] ?? 'Telefone desconhecido'}', // Aqui estão os telefones dos usuários.
-                                style: TextStyle(fontSize: 24.0),
-                              ),
-                            ],
+                return InkWell(
+                  onTap: () async {
+                    bool confirm = await showDialog(
+                      context: context,
+                      builder: (context) => AlertDialog(
+                        title: Text('Confirmação'),
+                        content: Text('Deseja obter a localização deste dentista?'),
+                        actions: <Widget>[
+                          TextButton(
+                            child: Text('NÃO'),
+                            onPressed: () {
+                              Navigator.of(context).pop(false);
+                            },
                           ),
-                        ),
-                      ],
+                          TextButton(
+                            child: Text('SIM'),
+                            onPressed: () {
+                              Navigator.of(context).pop(true);
+                            },
+                          ),
+                        ],
+                      ),
+                    );
+                    if (confirm == true) {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => LocView()),
+                      );
+                    }
+                  },
+                  child: Card(
+                    margin: EdgeInsets.all(10.0),
+                    child: Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: Row(
+                        children: [
+                          CircleAvatar(
+                            backgroundImage: AssetImage('assets/images/default_user.png'),
+                            radius: 30.0,
+                          ),
+                          SizedBox(width: 10.0),
+                          Flexible(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Nome: ${_firebaseData[index]['name'] ?? 'Nome desconhecido'}',
+                                  style: TextStyle(fontSize: 24.0),
+                                ),
+                                Text(
+                                  'Telefone: ${_firebaseData[index]['phone'] ?? 'Telefone desconhecido'}',
+                                  style: TextStyle(fontSize: 24.0),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 );
               },
-            ) :
-            Center(
+            )
+                : Center(
               child: Text(
                 'Aguarde...',
                 style: TextStyle(fontSize: 24.0),
---
+++ lib/loc_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LocView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("LocView"),
+      ),
+      body: Center(
+        child: Text('Hello World'),
+      ),
+    );
+  }
+}
